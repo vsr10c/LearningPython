c04974bd1bd892ee8c960c198146fa14acf4f6b2
vsr10c <vickyv268@gmail.com>
2018-09-09 19:16:25 +0530

    Adding lecture 9 to repo

---
+++ Lecture9.py
@@ -0,0 +1,29 @@
+# String and Escape Sequence Review
+
+# Print a string surrounding single quotes
+varStrOne = 'Single quots string'
+print(varStrOne)
+
+# prinr a string surrounded by double quotes
+varStrTwo = "Double quots string"
+print(varStrTwo)
+
+# Using escape sequence
+varStrThree = "This should be \"in quots\"."
+print(varStrThree)
+
+# Create a variable and assign it the string "Alligater"
+
+varStrFour = "Alligator"
+
+# Access "a" from variable varStrFour using indexing and print it.
+print(varStrFour[5])
+
+# Print the slice "Alli" from variable varStrFour
+print(varStrFour[:4])
+
+# Print the slice "gat" from variable varStrFour
+print(varStrFour[4:7])
+
+# Print the sliace "tor" from variable varStrFour
+print(varStrFour[6:])
