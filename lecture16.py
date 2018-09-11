a13a6e761d1e0cc01a55dddbf940c8d8194654df
vsr10c <vickyv268@gmail.com>
2018-09-11 20:21:32 +0530

    Addming leture 16 to repo

---
+++ lecture16.py
@@ -0,0 +1,25 @@
+varAnTrue = 3 > 2 and 4 > 3
+print(varAnTrue)
+
+varAnFalse = 2 > 3 and 4 > 3
+print(varAnFalse)
+
+varOrTrue = 2 > 3 or 4 > 3
+print(varOrTrue)
+
+varOrFalse = 2 > 3 or 3 > 4
+print(varOrFalse)
+
+varNoTrue = not 3 < 2
+print(varNoTrue)
+
+varNoFalse = not 3 >2
+print(varNoFalse)
+
+
+
+varOne = not 3 > 1 and 5 != 2 or 6 == 6
+print("var1 =", varOne)
+
+varTwo = 4 * 2 != 6 and 7 % 6 == 1
+print("Var2 =", varTwo)
