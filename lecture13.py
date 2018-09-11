6f2c320051d697099368647a882f34c599f4e9dc
vsr10c <vickyv268@gmail.com>
2018-09-11 19:44:21 +0530

    Adding lecture 13 to repo

---
+++ lecture13.py
@@ -0,0 +1,46 @@
+# Flow Control and Comparators:
+
+# 1.create a variable and assign to it an expression that is true using the greater than comparator
+varCompOne = 3 > 2
+print(varCompOne)
+
+# 2.create a variable and assign to it an expression that is false using the greater than comparator
+varCompTwo = 2 > 3
+print(varCompTwo)
+
+# 3.create a variable and assign to it an expression that is true using the greater than or equal to comparator
+varCompThree = 3 >= 3
+print(varCompThree)
+
+# 4.create a variable and assign to it an expression that is false using the greater than or equal to comparator
+varCompFour = 3 >= 4
+print(varCompFour)
+
+# 5.create a variable and assign to it an expression that is true using the less than comparator
+varLesCompOne = 2 < 3
+print(varLesCompOne)
+
+# 6.create a variable and assign to it an expression that is false using the less than comparator
+varLesCompTwo = 3 < 2
+print(varLesCompTwo)
+
+# 7.create a variable and assign to it an expression that is true using the less than or equal to comparator
+varLesCompThree = 3 <= 3
+print(varLesCompThree)
+
+# 8.create a variable and assign to it an expression that is false using the less than or equal to comparator
+varlesCompFour = 3 <= 2
+print(varlesCompFour)
+
+# 9.create a variable and assign to it an expression that is true using the equal to comparator
+varEqlCompOne = 3 == 3
+print(varEqlCompOne)
+
+# 10.create a variable and assign to it an expression that is false using the equal to comparator
+varEqlCompTwo = 3 == 2
+print(varEqlCompTwo)
+# 11.create a variable and assign to it an expression that is true using the not equal to comparator
+varNqlCompOne = 3 != 2
+print(varNqlCompOne)
+# 12.create a variable and assign to it an expression that is false using the not equal to comparator
+varNqlCompTwo = 3 != 3
