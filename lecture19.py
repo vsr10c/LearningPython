c25bfc21c5a865cf75ce1798821bf9363bd4e220
vsr10c <vickyv268@gmail.com>
2018-09-15 02:43:13 +0530

    Adding code in lecture 19

---
+++ lecture19.py
@@ -0,0 +1,10 @@
+varUName = input("Enter your name:")
+
+varUNameLen = len(varUName)
+
+if varUNameLen < 4:
+    print("Your name is less than 4 letters.")
+elif varUNameLen <= 10:
+    print("Your name is at least 4 letters but less than 10 letters.")
+else:
+    print("Your name is very long.")
